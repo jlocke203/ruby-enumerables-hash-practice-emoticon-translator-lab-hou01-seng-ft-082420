1612878087c4621674cead86bee0b00d3f1e818a
Jaden Locke <jadenlocke1113@gmail.com>
2020-07-15 21:14:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,7 @@ end
 def get_english_meaning(file_path, emoticon)
   hash_library = load_library(file_path)
   hash_library.each do |key, value|
-    if value[:japanese] == emoticon 
+    if key[:japanese] == emoticon 
   return value[:english]
 end
 end 
